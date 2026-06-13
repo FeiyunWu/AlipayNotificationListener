ed0f7e9f23bf1a11adcd3d6fc0bff4e1681b275a
FeiyunWu <feiyunwu@github.com>
2026-06-13 21:23:05 +0800

    Fix: remove deprecated @android:drawable/editbox_background, restore modern AGP/Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.zfblistener"
-    compileSdk = 30
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.zfblistener"
         minSdk = 23
-        targetSdk = 30
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
@@ -18,22 +18,21 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
-    implementation("androidx.preference:preference-ktx:1.1.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version "4.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
